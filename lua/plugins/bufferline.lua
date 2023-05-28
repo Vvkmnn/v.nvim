7bae0fbff337b1e4f4cc8bb84274664eda7690ca
Vivek Menon <mail@vvkmnn.xyz>
2023-05-28 02:47:44 -0400

    FIX(bufferline): Padded slant not working

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,6 +4,25 @@ return {
 		"akinsho/bufferline.nvim",
 		version = "v3.*",
 		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {},
+		opts = {
+				-- separator_style = "padded_slant",
+		},
+		-- config = function()
+		-- 	require("bufferline").setup({
+		-- 		separator_style = "padded_slant"
+		-- 		-- vim.cmd([[
+		-- 		-- 	augroup MyColors
+		-- 		-- 	autocmd!
+		-- 		--
+		-- 		-- 	autocmd ColorScheme * highlight BufferLineFill guibg=#191724
+		-- 		--
+		-- 		-- 	autocmd ColorScheme * highlight BufferLineSeparator guifg=#191724
+		-- 		--
+		-- 		-- 	autocmd ColorScheme * highlight BufferLineSeparatorSelected guifg=#191724
+		-- 		--
+		-- 		-- 	augroup END
+		-- 		-- 	]])
+		-- 	})
+		-- end
 	},
 }
