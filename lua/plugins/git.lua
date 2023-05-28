39b1ecfa398e503687125bd8155c1e6409bd40de
Vivek Menon <mail@vvkmnn.xyz>
2023-05-28 02:14:37 -0400

    FIX(neogit): Move into git.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,27 +5,43 @@ return {
 		opts = {},
 	},
 	{
+		-- Resolve conflicts with cX, jump with ]x
 		"akinsho/git-conflict.nvim",
 		commit = "2957f74",
 		config = function()
 			require("git-conflict").setup({
-				default_mappings = {
-					ours = "co",
-					theirs = "ct",
-					none = "c0",
-					both = "cb",
-					next = "cn",
-					prev = "cp",
-				},
+				-- default_mappings = {
+				-- 	ours = "co",
+				-- 	theirs = "ct",
+				-- 	none = "c0",
+				-- 	both = "cb",
+				-- 	next = "cn",
+				-- 	prev = "cp",
+				-- },
 			})
 		end,
 	},
 	{
 		"tpope/vim-fugitive",
-		config = function ()
+		config = function()
 			local map = require("helpers.keys").map
 			map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage the current file")
 			map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show the blame")
-		end
-	}
+		end,
+	},
+	{
+		-- Magit for Vim
+		"TimUntersberger/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("neogit").setup({
+				use_magit_keybindings = true,
+			})
+
+			local map = require("helpers.keys").map
+			map("n", "<leader>gg", "<cmd>Neogit<CR>", "Run Neogit")
+		end,
+	},
 }
