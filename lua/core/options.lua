8cfe5ad3a0db701d03db8792e4306ee61dece946
Vivek Menon <mail@vvkmnn.xyz>
2023-05-28 23:36:43 -0400

    MERGE(origin): Some artifacts from debian

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -31,3 +31,19 @@ augroup VCenterCursor
 augroup END
 ]])
 
+-- handle WSL (Windows for Linux) clipboard
+-- choco install win31yank
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
